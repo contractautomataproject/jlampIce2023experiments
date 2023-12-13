0c4cad2eecde3d0a1dd73b882d52873192287e0b
Davide Basile <davide.basile@isti.cnr.it>
2023-12-13 18:44:57 +0100

    first

--- src/main/java/io/github/contractautomata/experimentsJLAMPICE2023/Main.java
+++ src/main/java/io/github/contractautomata/experimentsJLAMPICE2023/Main.java
@@ -1,7 +1,29 @@
 package io.github.contractautomata.experimentsJLAMPICE2023;
 
+import io.github.contractautomata.catlib.automaton.Automaton;
+import io.github.contractautomata.catlib.automaton.label.CALabel;
+import io.github.contractautomata.catlib.automaton.label.action.Action;
+import io.github.contractautomata.catlib.automaton.state.State;
+import io.github.contractautomata.catlib.automaton.transition.ModalTransition;
+import io.github.contractautomata.catlib.converters.AutDataConverter;
+import io.github.contractautomata.catlib.operations.MSCACompositionFunction;
+import io.github.contractautomata.catlib.operations.NewOrchestrationSynthesisOperator;
+import io.github.contractautomata.catlib.requirements.StrongAgreement;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static final String dir = System.getProperty("user.dir")+ File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator;
+
+    public static void main(String[] args) throws IOException {
+        AutDataConverter<CALabel> bdc = new AutDataConverter<>(CALabel::new);
+
+        Automaton<String, Action, State<String>, ModalTransition<String,Action,State<String>, CALabel>> dealer = bdc.importMSCA(dir + "Dealer.data");
+        Automaton<String,Action,State<String>,ModalTransition<String,Action,State<String>,CALabel>> player = bdc.importMSCA(dir + "Player.data");
+        Automaton<String,Action,State<String>,ModalTransition<String,Action,State<String>,CALabel>> orc = new NewOrchestrationSynthesisOperator(new StrongAgreement()).apply(List.of(dealer,player,player));
+
+        System.out.println(orc);
     }
 }
